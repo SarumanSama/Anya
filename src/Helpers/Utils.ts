d30bb195bc39b4d75f4dc4b3efd7935d1a22fee5
alensaito1 <erwinsmithscout1@gmail.com>
2021-10-09 21:30:44 +0530

    Update Utils

--- src/Helpers/Utils.ts
+++ src/Helpers/Utils.ts
@@ -1,7 +1,12 @@
-import { readdirSync, statSync } from 'fs'
+import { readdirSync, statSync, unlinkSync } from 'fs'
 import { join } from 'path'
 import axios from 'axios'
 import { format } from 'util'
+import { randomBytes } from 'crypto'
+import { tmpdir } from 'os'
+import ffmpeg from 'fluent-ffmpeg'
+import { writeFile, readFile, unlink } from 'fs/promises'
+
 export default class Util {
     public format = format
 
@@ -35,4 +40,62 @@ export default class Util {
     public getRandomFloat = (min: number, max: number): number => {
         return Math.random() * (max - min) + min
     }
+
+    public getRandomItem = <T>(array: T[]): T => array[this.getRandomInt(0, array.length - 1)]
+
+    public getRandomItems = <T>(array: T[], count: number): T[] => {
+        return new Array(count).fill(0).map(() => this.getRandomItem(array))
+    }
+
+    public swap = <T>(array: T[], index1: number, index2: number): T[] => {
+        ;[array[index1], array[index2]] = [array[index2], array[index1]]
+        return array
+    }
+
+    public shuffle = <T>(array: T[]): T[] => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = this.getRandomInt(0, i)
+            this.swap(array, i, j)
+        }
+        return array
+    }
+
+    public getSecureRandom = (length: number): string => randomBytes(length).toString('hex')
+
+    public getRandomFilename = (ext = '', path = tmpdir()): string => {
+        const filename = this.getSecureRandom(16)
+        return join(path, `${filename}.${ext}`)
+    }
+
+    public getRandomString = (length: number): string => {
+        const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        return new Array(length)
+            .fill(0)
+            .map(() => this.getRandomItem(chars.split('')))
+            .join('')
+    }
+
+    public getUrls = (url: string): Set<string> => {
+        const urls = new Set<string>()
+        const regex = /(https?:\/\/[^\s]+)/g
+        let match: RegExpExecArray | null
+        while ((match = regex.exec(url)) !== null) {
+            urls.add(match[1])
+        }
+        return urls
+    }
+
+    public convertGIFToMP4 = async (gif: Buffer): Promise<Buffer> => {
+        const filename = this.getRandomFilename('gif')
+        await writeFile(filename, gif)
+        const output = filename.replace(/gif$/, 'mp4')
+        await new Promise((resolve, reject) => {
+            ffmpeg(filename).output(output).on('end', resolve).on('error', reject).run()
+        })
+        const buffer = await readFile(output)
+        Promise.all([unlink(filename), unlink(output)])
+        return buffer
+    }
+
+    public capitalize = (str: string): string => str.charAt(0).toUpperCase().concat(str.slice(1))
 }
